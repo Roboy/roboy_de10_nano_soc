f07f25a1c9f8428db18175dbcb4dfa1abd523605
letrend <simon.trendel@tum.de>
2017-09-07 13:58:54 +0200

    added two i2c buses for joint angles sensors

--- DE10_NANO_SoC_GHRD.v
+++ DE10_NANO_SoC_GHRD.v
@@ -127,10 +127,13 @@ soc_system u0(
                //Clock&Reset
                .clk_clk(FPGA_CLK1_50),                                      //                            clk.clk
                .reset_reset_n(hps_fpga_reset_n),                            //                          reset.reset_n
-					.darkroom_0_sensor_signals_sensor_signal_i({GPIO_0[25:10],GPIO_1[25:10]}),
+					.darkroom_0_sensor_signals_sensor_signal_i({GPIO_0[23:10],GPIO_1[23:10]}),  // only 28 pins connected
 					.darkroom_0_sensor_signals_led(fpga_led_internal),
-					.i2c_0_conduit_end_scl(ARDUINO_IO[0]),
-					.i2c_0_conduit_end_sda(ARDUINO_IO[1]),
+					// angle sensors 
+					.i2c_0_conduit_end_scl(GPIO_0[24]),
+					.i2c_0_conduit_end_sda(GPIO_0[25]),
+					.i2c_1_conduit_end_scl(GPIO_1[24]),
+					.i2c_1_conduit_end_sda(GPIO_1[25]),
 					.myocontrol_0_conduit_end_miso(GPIO_1[0]),
 					.myocontrol_0_conduit_end_mosi(GPIO_1[1]),
 					.myocontrol_0_conduit_end_sck(GPIO_1[2]),
