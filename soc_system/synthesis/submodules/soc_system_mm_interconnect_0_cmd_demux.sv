662f36edc8a24bbbebf0701d335af96671879c8c
letrend <st@roboy.org>
2018-09-09 23:09:30 +0200

    with more i2c cores for magnetic sensors

--- soc_system/synthesis/submodules/soc_system_mm_interconnect_0_cmd_demux.sv
+++ soc_system/synthesis/submodules/soc_system_mm_interconnect_0_cmd_demux.sv
@@ -1,20 +1,20 @@
-// (C) 2001-2017 Intel Corporation. All rights reserved.
+// (C) 2001-2018 Intel Corporation. All rights reserved.
 // Your use of Intel Corporation's design tools, logic functions and other 
 // software and tools, and its AMPP partner logic functions, and any output 
-// files any of the foregoing (including device programming or simulation 
+// files from any of the foregoing (including device programming or simulation 
 // files), and any associated documentation or information are expressly subject 
 // to the terms and conditions of the Intel Program License Subscription 
-// Agreement, Intel MegaCore Function License Agreement, or other applicable 
+// Agreement, Intel FPGA IP License Agreement, or other applicable 
 // license agreement, including, without limitation, that your use is for the 
 // sole purpose of programming logic devices manufactured by Intel and sold by 
 // Intel or its authorized distributors.  Please refer to the applicable 
 // agreement for further details.
 
 
-// $Id: //acds/rel/17.0std/ip/merlin/altera_merlin_demultiplexer/altera_merlin_demultiplexer.sv.terp#1 $
+// $Id: //acds/rel/18.0std/ip/merlin/altera_merlin_demultiplexer/altera_merlin_demultiplexer.sv.terp#1 $
 // $Revision: #1 $
-// $Date: 2017/01/22 $
-// $Author: swbranch $
+// $Date: 2018/01/31 $
+// $Author: psgswbuild $
 
 // -------------------------------------
 // Merlin Demultiplexer
@@ -29,9 +29,9 @@
 // Generation parameters:
 //   output_name:         soc_system_mm_interconnect_0_cmd_demux
 //   ST_DATA_W:           118
-//   ST_CHANNEL_W:        9
-//   NUM_OUTPUTS:         9
-//   VALID_WIDTH:         9
+//   ST_CHANNEL_W:        11
+//   NUM_OUTPUTS:         11
+//   VALID_WIDTH:         11
 // ------------------------------------------
 
 //------------------------------------------
@@ -45,9 +45,9 @@ module soc_system_mm_interconnect_0_cmd_demux
     // -------------------
     // Sink
     // -------------------
-    input  [9-1      : 0]   sink_valid,
+    input  [11-1      : 0]   sink_valid,
     input  [118-1    : 0]   sink_data, // ST_DATA_W=118
-    input  [9-1 : 0]   sink_channel, // ST_CHANNEL_W=9
+    input  [11-1 : 0]   sink_channel, // ST_CHANNEL_W=11
     input                         sink_startofpacket,
     input                         sink_endofpacket,
     output                        sink_ready,
@@ -57,66 +57,80 @@ module soc_system_mm_interconnect_0_cmd_demux
     // -------------------
     output reg                      src0_valid,
     output reg [118-1    : 0] src0_data, // ST_DATA_W=118
-    output reg [9-1 : 0] src0_channel, // ST_CHANNEL_W=9
+    output reg [11-1 : 0] src0_channel, // ST_CHANNEL_W=11
     output reg                      src0_startofpacket,
     output reg                      src0_endofpacket,
     input                           src0_ready,
 
     output reg                      src1_valid,
     output reg [118-1    : 0] src1_data, // ST_DATA_W=118
-    output reg [9-1 : 0] src1_channel, // ST_CHANNEL_W=9
+    output reg [11-1 : 0] src1_channel, // ST_CHANNEL_W=11
     output reg                      src1_startofpacket,
     output reg                      src1_endofpacket,
     input                           src1_ready,
 
     output reg                      src2_valid,
     output reg [118-1    : 0] src2_data, // ST_DATA_W=118
-    output reg [9-1 : 0] src2_channel, // ST_CHANNEL_W=9
+    output reg [11-1 : 0] src2_channel, // ST_CHANNEL_W=11
     output reg                      src2_startofpacket,
     output reg                      src2_endofpacket,
     input                           src2_ready,
 
     output reg                      src3_valid,
     output reg [118-1    : 0] src3_data, // ST_DATA_W=118
-    output reg [9-1 : 0] src3_channel, // ST_CHANNEL_W=9
+    output reg [11-1 : 0] src3_channel, // ST_CHANNEL_W=11
     output reg                      src3_startofpacket,
     output reg                      src3_endofpacket,
     input                           src3_ready,
 
     output reg                      src4_valid,
     output reg [118-1    : 0] src4_data, // ST_DATA_W=118
-    output reg [9-1 : 0] src4_channel, // ST_CHANNEL_W=9
+    output reg [11-1 : 0] src4_channel, // ST_CHANNEL_W=11
     output reg                      src4_startofpacket,
     output reg                      src4_endofpacket,
     input                           src4_ready,
 
     output reg                      src5_valid,
     output reg [118-1    : 0] src5_data, // ST_DATA_W=118
-    output reg [9-1 : 0] src5_channel, // ST_CHANNEL_W=9
+    output reg [11-1 : 0] src5_channel, // ST_CHANNEL_W=11
     output reg                      src5_startofpacket,
     output reg                      src5_endofpacket,
     input                           src5_ready,
 
     output reg                      src6_valid,
     output reg [118-1    : 0] src6_data, // ST_DATA_W=118
-    output reg [9-1 : 0] src6_channel, // ST_CHANNEL_W=9
+    output reg [11-1 : 0] src6_channel, // ST_CHANNEL_W=11
     output reg                      src6_startofpacket,
     output reg                      src6_endofpacket,
     input                           src6_ready,
 
     output reg                      src7_valid,
     output reg [118-1    : 0] src7_data, // ST_DATA_W=118
-    output reg [9-1 : 0] src7_channel, // ST_CHANNEL_W=9
+    output reg [11-1 : 0] src7_channel, // ST_CHANNEL_W=11
     output reg                      src7_startofpacket,
     output reg                      src7_endofpacket,
     input                           src7_ready,
 
     output reg                      src8_valid,
     output reg [118-1    : 0] src8_data, // ST_DATA_W=118
-    output reg [9-1 : 0] src8_channel, // ST_CHANNEL_W=9
+    output reg [11-1 : 0] src8_channel, // ST_CHANNEL_W=11
     output reg                      src8_startofpacket,
     output reg                      src8_endofpacket,
     input                           src8_ready,
+
+    output reg                      src9_valid,
+    output reg [118-1    : 0] src9_data, // ST_DATA_W=118
+    output reg [11-1 : 0] src9_channel, // ST_CHANNEL_W=11
+    output reg                      src9_startofpacket,
+    output reg                      src9_endofpacket,
+    input                           src9_ready,
+
+    output reg                      src10_valid,
+    output reg [118-1    : 0] src10_data, // ST_DATA_W=118
+    output reg [11-1 : 0] src10_channel, // ST_CHANNEL_W=11
+    output reg                      src10_startofpacket,
+    output reg                      src10_endofpacket,
+    input                           src10_ready,
 
 
     // -------------------
@@ -129,7 +143,7 @@ module soc_system_mm_interconnect_0_cmd_demux
 
 );
 
-    localparam NUM_OUTPUTS = 9;
+    localparam NUM_OUTPUTS = 11;
     wire [NUM_OUTPUTS - 1 : 0] ready_vector;
 
     // -------------------
@@ -199,6 +213,20 @@ module soc_system_mm_interconnect_0_cmd_demux
 
         src8_valid         = sink_channel[8] && sink_valid[8];
 
+        src9_data          = sink_data;
+        src9_startofpacket = sink_startofpacket;
+        src9_endofpacket   = sink_endofpacket;
+        src9_channel       = sink_channel >> NUM_OUTPUTS;
+
+        src9_valid         = sink_channel[9] && sink_valid[9];
+
+        src10_data          = sink_data;
+        src10_startofpacket = sink_startofpacket;
+        src10_endofpacket   = sink_endofpacket;
+        src10_channel       = sink_channel >> NUM_OUTPUTS;
+
+        src10_valid         = sink_channel[10] && sink_valid[10];
+
     end
 
     // -------------------
@@ -213,6 +241,8 @@ module soc_system_mm_interconnect_0_cmd_demux
     assign ready_vector[6] = src6_ready;
     assign ready_vector[7] = src7_ready;
     assign ready_vector[8] = src8_ready;
+    assign ready_vector[9] = src9_ready;
+    assign ready_vector[10] = src10_ready;
 
     assign sink_ready = |(sink_channel & ready_vector);
 
