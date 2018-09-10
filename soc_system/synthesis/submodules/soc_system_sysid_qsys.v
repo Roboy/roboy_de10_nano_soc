e1a7ae4a28efc73d75ac72ff5eeff70f57b713c0
letrend <simon.trendel@tum.de>
2018-09-10 21:08:22 +0200

    ignoring unacknowleged sensor values

--- soc_system/synthesis/submodules/soc_system_sysid_qsys.v
+++ soc_system/synthesis/submodules/soc_system_sysid_qsys.v
@@ -49,7 +49,7 @@ module soc_system_sysid_qsys (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1536525267 : 3735928559;
+  assign readdata = address ? 1536601680 : 3735928559;
 
 endmodule
 
