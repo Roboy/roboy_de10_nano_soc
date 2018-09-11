a2581b7e6f48cd658fa0bdfe4a83800bb172007d
letrend <simon.trendel@tum.de>
2018-09-12 00:47:15 +0200

    outsourced elbow 1 dof and hand control, hand control untested yet

--- soc_system/synthesis/submodules/soc_system_sysid_qsys.v
+++ soc_system/synthesis/submodules/soc_system_sysid_qsys.v
@@ -49,7 +49,7 @@ module soc_system_sysid_qsys (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1536620238 : 3735928559;
+  assign readdata = address ? 1536702995 : 3735928559;
 
 endmodule
 
