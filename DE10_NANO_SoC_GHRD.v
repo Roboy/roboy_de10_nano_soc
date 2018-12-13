59d88d7f575a3fa1dfa0109ff67f866447586774
letrend <simon.trendel@tum.de>
2018-12-13 01:38:04 +0100

    added myo spi

--- DE10_NANO_SoC_GHRD.v
+++ DE10_NANO_SoC_GHRD.v
@@ -154,24 +154,24 @@ soc_system u0(
 						.i2c_2_conduit_end_scl(GPIO_0[31]),
 						.i2c_2_conduit_end_sda(GPIO_0[30]), 
 //						// myocontrol, three buses (9/4/3 motors)
-//						.myocontrol_0_conduit_end_mosi(GPIO_0[0]),
-//						.myocontrol_0_conduit_end_miso(GPIO_0[1]),
-//						.myocontrol_0_conduit_end_sck(GPIO_0[2]),
-//						.myocontrol_0_conduit_end_ss_n_o({GPIO_0[11:3]}),
-//						.myocontrol_0_conduit_end_mirrored_muscle_unit(SW[2]==1 && SW[1]==0 && SW[0]==0), // true for switch ID 4
-//						.myocontrol_0_conduit_end_power_sense_n(GPIO_0[19] && SW[3]),
-//						.myocontrol_1_conduit_end_mosi(GPIO_0[12]), 
-//						.myocontrol_1_conduit_end_miso(GPIO_0[13]),
-//						.myocontrol_1_conduit_end_sck(GPIO_0[14]),
-//						.myocontrol_1_conduit_end_ss_n_o({GPIO_0[21:20], GPIO_0[18:15]}),
-//						.myocontrol_1_conduit_end_mirrored_muscle_unit(SW[2]==1 && SW[1]==0 && SW[0]==0), // true for switch ID 4
-//						.myocontrol_1_conduit_end_power_sense_n(GPIO_0[19] && SW[3]),
-//						.myocontrol_1_conduit_end_arm_scl(GPIO_0[33]),
-//						.myocontrol_1_conduit_end_arm_sda(GPIO_0[31]),
-//						.myocontrol_1_conduit_end_angle_mosi(GPIO_0[22]),
-//						.myocontrol_1_conduit_end_angle_miso(GPIO_0[23]),
-//						.myocontrol_1_conduit_end_angle_sck(GPIO_0[24]),
-//						.myocontrol_1_conduit_end_angle_ss_n_o(GPIO_0[30:25]),
+						.myocontrol_0_conduit_end_mosi(GPIO_0[0]),
+						.myocontrol_0_conduit_end_miso(GPIO_0[1]),
+						.myocontrol_0_conduit_end_sck(GPIO_0[2]),
+						.myocontrol_0_conduit_end_ss_n_o({GPIO_0[11:3]}),
+						.myocontrol_0_conduit_end_mirrored_muscle_unit(SW[2]==1 && SW[1]==0 && SW[0]==0), // true for switch ID 4
+						.myocontrol_0_conduit_end_power_sense_n(GPIO_0[19] && SW[3]),
+						.myocontrol_1_conduit_end_mosi(GPIO_0[12]), 
+						.myocontrol_1_conduit_end_miso(GPIO_0[13]),
+						.myocontrol_1_conduit_end_sck(GPIO_0[14]),
+						.myocontrol_1_conduit_end_ss_n_o({GPIO_0[21:20], GPIO_0[18:15]}),
+						.myocontrol_1_conduit_end_mirrored_muscle_unit(SW[2]==1 && SW[1]==0 && SW[0]==0), // true for switch ID 4
+						.myocontrol_1_conduit_end_power_sense_n(GPIO_0[19] && SW[3]),
+						.myocontrol_1_conduit_end_arm_scl(GPIO_0[33]),
+						.myocontrol_1_conduit_end_arm_sda(GPIO_0[31]),
+						.myocontrol_1_conduit_end_angle_mosi(GPIO_0[22]),
+						.myocontrol_1_conduit_end_angle_miso(GPIO_0[23]),
+						.myocontrol_1_conduit_end_angle_sck(GPIO_0[24]),
+						.myocontrol_1_conduit_end_angle_ss_n_o(GPIO_0[30:25]),
 					`else /*USE_ETHERNET_SIDE*/
 //						// darkroom
 //						.darkroom_0_conduit_end_d_io({GPIO_0[34],GPIO_0[32],GPIO_0[30],GPIO_0[28],GPIO_0[26],GPIO_0[24],GPIO_0[22],GPIO_0[20],GPIO_0[18],GPIO_0[16],GPIO_0[14],GPIO_0[12],GPIO_0[10],GPIO_0[8],GPIO_0[6],GPIO_0[4]}),
@@ -300,7 +300,7 @@ soc_system u0(
                .hps_0_f2h_debug_reset_req_reset_n(~hps_debug_reset),        //      hps_0_f2h_debug_reset_req.reset_n
                .hps_0_f2h_warm_reset_req_reset_n(~hps_warm_reset),          //       hps_0_f2h_warm_reset_req.reset_n
 					//PIO
-					.pio_0_external_connection_export(GPIO_0[0])              //    pio_0_external_connection.export
+					.pio_0_external_connection_export(GPIO_1[0])              //    pio_0_external_connection.export
            );
 //
 // Debounce logic to clean out glitches within 1ms
