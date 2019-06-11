2dc8f02763d455df9bb8ab5439818fb0531b378d
letrend <st@gi.ai>
2019-06-11 21:03:28 +0200

    ok, i'm stupid

--- soc_system/synthesis/submodules/soc_system_sysid_qsys.v
+++ soc_system/synthesis/submodules/soc_system_sysid_qsys.v
@@ -49,7 +49,7 @@ module soc_system_sysid_qsys (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1560270983 : 114415;
+  assign readdata = address ? 1560275777 : 114415;
 
 endmodule
 
