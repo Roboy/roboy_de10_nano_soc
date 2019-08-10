7daec41a989cd98cb2a4b15ec2c183b9538f33ac
letrend <st@gi.ai>
2019-08-10 03:11:06 +0200

    snaity checks are for looooooosers

--- soc_system/synthesis/submodules/soc_system_sysid_qsys.v
+++ soc_system/synthesis/submodules/soc_system_sysid_qsys.v
@@ -49,7 +49,7 @@ module soc_system_sysid_qsys (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1565396118 : 114415;
+  assign readdata = address ? 1565397101 : 114415;
 
 endmodule
 
