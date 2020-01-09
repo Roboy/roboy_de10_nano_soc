9dd3df5384814e7d17cf9b14b9ded3e7a2162ec1
letrend <st@gi.ai>
2020-01-09 20:41:19 +0100

    with neopixel

--- soc_system/synthesis/submodules/soc_system_sysid_qsys.v
+++ soc_system/synthesis/submodules/soc_system_sysid_qsys.v
@@ -49,7 +49,7 @@ module soc_system_sysid_qsys (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1577974897 : 2976579765;
+  assign readdata = address ? 1578317036 : 2976579765;
 
 endmodule
 
